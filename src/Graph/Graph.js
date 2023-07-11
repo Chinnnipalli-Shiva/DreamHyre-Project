df238aca90c88b717221132aed4c46af57ca6289
Chinnipalli Shiva <chinnipallishiva26@gmail.com>
2023-07-11 10:15:37 +0530

    updated code

--- src/Graph/Graph.js
+++ src/Graph/Graph.js
@@ -4,66 +4,115 @@ import "./Graph.css";
 
 import {
   Chart as ChartsJS,
-  Title,
-  Tooltip,
   LineElement,
   Legend,
   CategoryScale, // x
   LinearScale, // y a
   PointElement,
-  Filler,
-  ArcElement,
 } from "chart.js";
 
 ChartsJS.register(
-  Title,
-  Tooltip,
-  ArcElement,
   LineElement,
   Legend,
   CategoryScale,
   LinearScale,
-  PointElement,
-  Filler
+  PointElement
 );
 
 function Graphs() {
-  const arr1 = [120,400,320,521,314,239,201,410,230,256,287,302];
-  const arr2 = [320,351,201,130,298,410,369,321,201,257,103,378];
+  const arr1 = [120, 400, 320, 521, 314, 239, 201, 410, 230, 256, 287, 302];
+  const arr = [320, 351, 201, 130, 298, 410, 369, 321, 201, 257, 103, 378];
 
-  let [info, updateinfo] = useState({
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep","Oct","Nov","Dec"],
+  let data = {
+    labels: [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      // "Aug",
+      // "Sep",
+      // "Oct",
+      // "Nov",
+      // "Dec",
+    ],
     datasets: [
       {
-        label: "Guest",
         data: arr1,
-        backgroundColor: "#9BDD7C",
+        backgroundColor: "transparent",
         borderColor: "#9BDD7C",
         tension: 0.4,
-        pointBorderColor: "#9BDD7C",
-        pointBackgroundColor: "#9BDD7C",
+        pointBorderColor: "transparent",
       },
       {
-        label: "User",
-        data: arr2,
-        backgroundColor: "#E9A0A0",
+        data: arr,
+        backgroundColor: "transparent",
         borderColor: "#E9A0A0",
         tension: 0.4,
-        pointBorderColor: "#E9A0A0",
-        pointBackgroundColor: "#E9A0A0",
+        pointBorderColor: "transparent",
       },
     ],
-  });
+  };
+
+  let options = {
+    plugins: {
+      legend: false,
+    },
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
+        barPercentage: 0.1,
+        categoryPercentage: 0.3,
+      },
+    },
+    y: {
+      min: 50,
+      max: 550,
+      ticks: {
+        stepSize: 110,
+      },
+    },
+  };
+
   return (
     <div className="graph">
       <div className="activity">
         <h3>Activity</h3>
         <p>May-June 2021</p>
       </div>
+
+
       <div className="line">
-        <Line data={info} style={{width:"700px",height:"310px"}} ></Line>
+        <Line className="org" data={data} options={options} style={{ width: "900px", height: "250px" }}></Line>
+      </div>
+
+
+
+      <div className="labelling">
+        <div className="graph-label">
+          <div className="label-flex">
+            <div className="color color-guest"></div>
+            <div className="labelName">
+              <p>Guest</p>
+            </div>
+          </div>
+        </div>
+        <div className="graph-label">
+          <div className="label-flex">
+            <div className="color color-user"></div>
+            <div className="labelName">
+              <p>User</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
 export default Graphs;
+
+//style={{ width: "900px", height: "250px" }}
